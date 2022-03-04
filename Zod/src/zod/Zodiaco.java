631f3f9823fc455efc1d1ed5805c53a36ea771ec
Angel CJ <acontreras185@ieszaidinvergeles.org>
2022-03-04 09:29:53 +0100

    Añadido JavaDoc 1

--- Zod/src/zod/Zodiaco.java
+++ Zod/src/zod/Zodiaco.java
@@ -1,133 +1,127 @@
 package zod;
 import java.util.Scanner;
 /**
- *
+ *<h1>A Java class used to know the Zodiac Sign knowing the day and the month, if the date is invalid, it returns NULL</h1>
+ * 
+ * @version 1.0, 03/03/2022
  * @author Angel
+ *
  */
 public class Zodiaco {
-    
-
+    /**
+     * 
+     */
     public static void mensajeZod(){
-
+        //Message shown when the sign is calculated
         int dia, mes;
         Scanner Sc = new Scanner(System.in);
-        System.out.print("Introduce el dia de nacimiento: ");
-        dia = Integer.parseInt(Sc.nextLine());
-        System.out.print("Introduce tu mes de nacimiento: ");
-        mes = Integer.parseInt(Sc.nextLine());
-        System.out.println("Tu signo es: " + calculaSigno(dia,mes));
-}
+        System.out.print("Introduce el dia de nacimiento: "); 
+        dia = Integer.parseInt(Sc.nextLine());//Specifies the day of the month (1-31)
+        System.out.print("Introduce tu mes de nacimiento: "); 
+        mes = Integer.parseInt(Sc.nextLine());//Specifies the month of the sign (1-12)
+        System.out.println("Tu signo es: " + calculaSigno(dia,mes));//String that contains the name of the sign
+        Sc.close();
+} 
     public static String calculaSigno(int dia, int mes) {
-        if (mes == 1) {
-            if (dia >= 21) {
-                /*acuario*/
-                return "acuario";
-            } else {
-                /*capricornio*/
-                return "capricornio";
+        /**@param dia Specifies the day of the month (1-31)
+        * @param mes Specifies the month of the sign (1-12)
+        * @param signo String that contains the name of the sign
+        * @return The name of the sign according to the date introduced
+        */
+        String signo = null;
+        switch (mes) {
+            case 1:
+                if (dia<=21)
+                    signo = "Capricornio";
+                else if (dia<=31)
+                    signo = "Acuario";
+                break;
+            case 2:
+                if (dia<=21)
+                    signo = "Acuario";
+                else if (dia<=29)
+                    signo = "Piscis";
+                break;
+            case 3:
+            if (dia <= 21) {
+                
+                signo = "Piscis";
+            } else if (dia<=31){
+                
+                signo = "Aries";
             }
-        }
-        if (mes == 2) {
-            if (dia >= 20) {
-                /**/
-                return "piscis";
-            } else {
-                /**/
-                return "acuario";
+        
+        case 4:
+                if (dia<=21)
+                    signo = "Aries";
+                else if (dia<=31)
+                    signo = "Tauro";
+                break;
+            case 5:
+                if (dia<=21)
+                    signo = "Tauro";
+                else if (dia<=29)
+                    signo = "Géminis";
+                break;
+            case 6:
+            if (dia <= 21) {
+                
+                signo = "Géminis";
+            } else if (dia<=31){
+                
+                signo = "Cáncer";
             }
-        }
-        if (mes == 3) {
-            if (dia >= 21) {
-                /**/
-                return "aries";
-            } else {
-                /**/
-                return "piscis";
+         case 7:
+                if (dia<=21)
+                    signo = "Cáncer";
+                else if (dia<=31)
+                    signo = "Leo";
+                break;
+            case 8:
+                if (dia<=21)
+                    signo = "Leo";
+                else if (dia<=29)
+                    signo = "Virgo";
+                break;
+            case 9:
+            if (dia <= 21) {
+                
+                signo = "Virgo";
+            } else if (dia<=31){
+                
+                signo = "Libra";
             }
-        }
-        if (mes == 4) {
-            if (dia >= 21) {
-                /**/
-                return "tauro";
-            } else {
-                /**/
-                return "Aries";
+         case 10:
+                if (dia<=21)
+                    signo = "Libra";
+                else if (dia<=31)
+                    signo = "Escorpio";
+                break;
+            case 11:
+                if (dia<=21)
+                    signo = "Escorpio";
+                else if (dia<=29)
+                    signo = "Sagitario";
+                break;
+            case 12:
+            if (dia <= 21) {
+                
+                signo = "Sagitario";
+            } else if (dia<=31){
+                
+                signo = "Capricornio";
             }
-        }
-        if (mes == 5) {
-            if (dia >= 21) {
-                /**/
-                return "geminis";
-            } else {
-                /**/
-                return "tauro";
             }
-        }
-        if (mes == 6) {
-            if (dia >= 22) {
-                /**/
-                return "cancer";
-            } else {
-                /**/
-                return "geminis";
+           
+            if (signo==null){
+                System.out.println("Los datos no son correctos");
             }
+                return signo;
         }
-        if (mes == 7) {
-            if (dia >= 22) {
-               /**/
-                return "leo";
-            } else {
-                /**/
-                return "cancer";
-            }
-        }
-        if (mes == 8) {
-            if (dia >= 24) {
-                /**/
-                return "virgo";
-            } else {
-                /**/
-                return "leo";
-            }
-        }
-        if (mes == 9) {
-            if (dia >= 24) {
-                /**/
-                return "libra";
-            } else {
-                /**/
-                return "virgo";
-            }
-        }
-        if (mes == 10) {
-            if (dia >= 23) {
-                /**/
-                return "escorpio";
-            } else {
-                /**/
-                return "libra";
-            }
-        }
-        if (mes == 11) {
-            if (dia >= 22) {
-                /**/
-                return "sagitario";
-            } else {
-                /**/
-                return "escorpio";
-            }
-        }
-        if (mes == 12) {
-            if (dia >= 22) {
-                /**/
-                return "capricornio";
-            } else {
-                /**/
-                return "sagitario";
-            }
-        }
-                /**/
-        return "Fecha inválida";
+
     }
-}
+
+
+
+
 
